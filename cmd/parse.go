2044b8074cce80f9a91f2cf76190dc274bc97b72
John Nguyen <jtnguyen236@gmail.com>
2016-06-22 21:04:19 +0800

    added flags to determine whether or not to parse literals

--- cmd/parse.go
+++ cmd/parse.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"log"
+
 	"github.com/nii236/btcer/parse"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -12,8 +14,15 @@ var parseCmd = &cobra.Command{
 	Short: "Begin parsing the blockchain",
 	Long: `Begin parsing the blockchain.
 
-This requires a full node BTCD instance.`,
+This requires a full node BTCD instance.
+
+By default, only the relations between types will be generated. Actual
+literals will only be generated when the corresponding flag is set.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 0 {
+			log.Fatalln("Invalid arguments")
+		}
 		start := viper.GetInt("start")
 		end := viper.GetInt("end")
 		parse.Fetch(start, end)
@@ -24,6 +33,12 @@ func init() {
 	RootCmd.AddCommand(parseCmd)
 	parseCmd.Flags().IntP("start", "s", 0, "Start block")
 	parseCmd.Flags().IntP("end", "e", 10000, "End block")
+	parseCmd.Flags().BoolP("block", "b", false, "Parse block literals")
+	parseCmd.Flags().BoolP("tx", "t", false, "Parse transaction literals")
+	parseCmd.Flags().BoolP("vin", "i", false, "Parse vin literals")
+	parseCmd.Flags().BoolP("vout", "o", false, "Parse vout literals")
+	parseCmd.Flags().BoolP("scriptpubkey", "p", false, "Parse scriptpubkey literals")
+	parseCmd.Flags().BoolP("address", "a", false, "Parse address literals")
 	viper.BindPFlags(parseCmd.Flags())
 
 }
